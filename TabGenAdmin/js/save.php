493a13f56e4e10c58fd97a0882929e29662398a2
bencin143 <bencin.bethees@gmail.com>
2017-02-04 13:23:03 +0530

    Add files via upload

---
+++ TabGenAdmin/js/save.php
@@ -0,0 +1,45 @@
+<?php
+include('connect_db.php');//connecting database here
+include('tabgen_php_functions.php');
+
+
+$or = $_POST['or'];  
+$ipadd = $_POST['ipadd'];
+$portadd = $_POST['portadd'];
+$datusr = $_POST['datusr'];
+$datpass = $_POST['datpass'];
+$boxes = $_POST['boxes'];
+
+$each_val = implode(',', $boxes);
+
+if($conn){
+	$time = time()*1000;
+	$id = randId(26);//creating unique id, this function is coded inside tabgen_php_functions.php file
+	$query = "insert into HISConnectivit1y(Id,CreateAt,UpdateAt,DeleteAt,OrganisationName,DataServerIPAdd,DataBasePortAddress,DatabaseUsername,DatabasePassword,Queries)
+	values('$id','$time','$time',0,'$or','$ipadd','$portadd','$datusr','$datpass','$each_val')";
+	if($conn->query($query)){
+			echo "Inserted successfully";
+		}
+		else{
+			echo "error";
+		}
+	}
+	else{
+		echo "Oops! An error with the query.";
+	}
+
+/*
+$result = mysqli_query('select OrganisationName,DataServerIPAdd,DatabaseUsername,DatabasePassword from HISConnectivity where values = "'. $or .','. $ipadd .','. $datusr .','. $datpass .'"');  
+  
+
+if(mysqli_num_rows($result)>0){  
+    
+    echo "tested ok";  
+}else{  
+   
+  
+    echo "error";  
+} 
+*/
+ 
+?>
